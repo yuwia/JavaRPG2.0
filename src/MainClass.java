d9842b2355ace78efe64c00c0e03ff9752b121d4
lolzyman <markkent777@gmail.com>
2017-03-15 11:24:53 -0400

    added some dots

--- src/MainClass.java
+++ src/MainClass.java
@@ -11,6 +11,6 @@ public class MainClass
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(512, 512);
         f.setResizable(false);
-        //Lets see if this does anyting
+        //Lets see if this does anything...
     }
 }
